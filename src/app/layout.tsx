637cc25fd523e4560ccdf46e371c5e828bfc9ee0
justnikee <hk5561111212@gmail.com>
2023-04-25 00:10:58 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -14,8 +14,25 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+<html lang="en">
+  <body className="{inter.className} flex justify-center">
+    <div className='max-w-[1240px] w-full'>
+      <nav className='flex items-center w-full h-[100px]'>
+        <div className='w-full flex items-center justify-start'>
+            <Link href={"/"}>Nikhil</Link>
+        </div>
+        <div className='w-[75%] flex itmes-center justify-end gap-10'>
+          <Link href={"/"}>Home</Link>
+          <Link href={"/about"}>About</Link>
+          <Link href={"/work"}>Work</Link>
+          <Link href={"/contact"}>Contact</Link>
+        </div>
+      </nav>
+        <div>
+          {children}
+        </div>
+    </div>
+  </body>
+</html>
   )
 }
